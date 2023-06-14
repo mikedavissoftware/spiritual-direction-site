8804f3552abf123ed0ddf4480954c06b44b59f14
Mike Davis <micdavis93@gmail.com>
2023-06-13 22:18:43 -0600

    routed logo to home page

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -9,7 +9,7 @@ export default function Footer() {
     <div>
       <div className="navbar bg-black">
         <div className="flex-1">
-          <a className="btn btn-ghost normal-case text-xl">Mike Davis - Spiritual Director</a>
+          <NavLink exact to="/" className="btn btn-ghost normal-case text-xl">Mike Davis - Spiritual Director</NavLink>
         </div>
         <div className="flex-none">
           <ul className="menu menu-horizontal px-1">
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
   return (
     <div className="navbar bg-black">
       <div className="flex-1">
-        <a className="btn btn-ghost normal-case text-xl">Mike Davis - Spiritual Director</a>
+        <NavLink exact to="/" className="btn btn-ghost normal-case text-xl">Mike Davis - Spiritual Director</NavLink>
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1">
