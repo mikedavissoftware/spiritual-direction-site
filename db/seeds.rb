b8bebef4a9e96c47ee4a6ef383ccefaea4c2c989
Mike Davis <micdavis93@gmail.com>
2023-06-22 15:46:22 -0600

    removed environment-specific contingency on admin user creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,20 @@
 p "🌱 Now Seeding..."
 
 p "Seeding Users..."
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create(
+  email: 'admin@example.com', 
+  password: 'password', 
+  password_confirmation: 'password'
+)
 
 p "Seeding Posts..."
 10.times do
-  Post.create!(
+  Post.create(
     title: Faker::Lorem.sentence(word_count: 5),
     body: Faker::Lorem.paragraph(sentence_count: 30),
     placeholder_image: Faker::LoremFlickr.image(size: "500x600", search_terms: ['sports'])
   )
+  p "Created Post"
 end
 
 p "🌱 Done Seeding!"
