009755e90cc99fe75d806402ffbbc41a56c342bf
Michael Davis <micdavis93@gmail.com>
2024-07-06 10:28:25 -0600

    small changes

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,34 +7,23 @@ export default function Header() {
 
 
   return (
-    <div className="navbar bg-black">
+    <div className="navbar bg-black p-4">
       <div className="avatar">
-        <div className="w-20 rounded-full">
+        <div className="w-20 rounded-full m-2">
           <img src={ProfilePicture} />
         </div>
       </div>
       <div className="flex-1">
-        <NavLink exact to="/" className="btn btn-ghost normal-case text-xl">Mike Davis, Spiritual Director</NavLink>
+        <NavLink exact to="/" className="btn btn-ghost normal-case text-xl">Mike Davis, Spiritual Therapist</NavLink>
       </div>
       <div className="flex-none">
-        <ul className="menu menu-horizontal px-1">
+        <ul id="nav-links" className="menu menu-horizontal">
           <li><NavLink exact to="/">Home</NavLink></li>
           <li><NavLink to="/about">About</NavLink></li>
           <li><NavLink to="/blog">Blog</NavLink></li>
           <li><NavLink to="/events">Events</NavLink></li>
           <li><NavLink to="/booking">Booking</NavLink></li>
           <li><NavLink to="/faq">FAQ</NavLink></li>
-          {/* <li>
-            <details>
-              <summary>
-                Parent
-              </summary>
-              <ul className="p-2 bg-black">
-                <li><a>Link 1</a></li>
-                <li><a>Link 2</a></li>
-              </ul>
-            </details>
-          </li> */}
         </ul>
       </div>
     </div>
